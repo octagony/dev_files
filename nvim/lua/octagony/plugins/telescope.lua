3c78be406b5cd5b79e534c7f53b09a74b0638515
octagony <moctagony@gmail.com>
2023-08-11 18:57:50 +0300

    [FEAT] Update telescope

--- nvim/lua/octagony/plugins/telescope.lua
+++ nvim/lua/octagony/plugins/telescope.lua
@@ -14,6 +14,20 @@ local telescope_actions = require("telescope.actions")
 
 require("telescope").setup({
 	defaults = {
+		path_display = { "truncate" },
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "bottom",
+				preview_width = 0.55,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
 		mappings = {
 			n = {
 				["q"] = telescope_actions.close,

e051c429f4d3088719f2abd38b4f0d6f22805467
octagony <moctagony@gmail.com>
2023-08-11 19:15:29 +0300

    [FEAT] Update webdev-icons file

--- nvim/lua/octagony/plugins/web-devicons.lua
+++ nvim/lua/octagony/plugins/web-devicons.lua
@@ -3,14 +3,57 @@ require("nvim-web-devicons").setup({
 		zsh = {
 			icon = "",
 			color = "#428850",
-			cterm_color = "65",
 			name = "Zsh",
 		},
 		astro = {
 			icon = "",
 			color = "#fab387",
-			cterm_color = "65",
 			name = "astro",
+		},
+		lock = {
+			icon = "󰌾",
+			color = "#5b4828",
+			name = "Lock",
+		},
+		mp3 = {
+			icon = "󰎆",
+			color = "#00a900",
+			name = "Mp3",
+		},
+		mp4 = {
+			icon = "",
+			color = "#520053",
+			name = "Mp4",
+		},
+		out = {
+			icon = "",
+			color = "#7c7c7c",
+			name = "Out",
+		},
+		["robots.txt"] = {
+			icon = "󰚩",
+			COLOR = "#f38ba8",
+			name = "Robots",
+		},
+		ttf = {
+			icon = "",
+			color = "#94e2d5",
+			name = "TrueTypeFont",
+		},
+		woff = {
+			icon = "",
+			color = "#b4befe",
+			name = "WebOpenFontFormat",
+		},
+		woff2 = {
+			icon = "",
+			color = "#89b4fa",
+			name = "WebOpenFontFormat2",
+		},
+		[".node-version"] = {
+			icon = "",
+			color = "#a6e3a1",
+			name = "NodeVersion",
 		},
 	},
 })
